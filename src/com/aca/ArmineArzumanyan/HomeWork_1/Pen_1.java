eac2210cfc7588d61759008f04e8fe2c98dbf6bc
ArzArm <armine.arzumanyan@gmail.com>
2020-06-25 14:29:00 +0400

    HomeWork1

---
+++ src/com/aca/ArmineArzumanyan/HomeWork_1/Pen_1.java
@@ -0,0 +1,12 @@
+package com.aca.ArmineArzumanyan.HomeWork_1;
+
+public class Pen_1 extends GadgetNoMemory {
+
+    public Pen_1(String name, int code) {
+        super(name, code);
+        price = 6500;
+        recDuration = 2;
+        gadgetType = GadgetType.VIDEO;
+    }
+
+}
---
+++ src/com/aca/ArmineArzumanyan/HomeWork_1/USB_2.java
@@ -0,0 +1,12 @@
+package com.aca.ArmineArzumanyan.HomeWork_1;
+
+public class USB_2 extends GadgetWithMemory {
+
+    public USB_2(String name, int cod) {
+        super(name, cod);
+        price = 11000;
+        recDuration = 9;
+        rum = 8;
+        gadgetType = GadgetType.RECORD;
+    }
+}
