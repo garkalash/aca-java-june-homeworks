40d6bd2b87c102d91b30ca60de33dd5d06388dbb
ArzArm <armine.arzumanyan@gmail.com>
2020-06-26 19:08:44 +0400

    HomeWork1

--- src/com/aca/ArmineArzumanyan/HomeWork_1/GadgetNoMemory.java
+++
@@ -1,9 +0,0 @@
-package com.aca.ArmineArzumanyan.HomeWork_1;
-
-public class GadgetNoMemory extends Gadget {
-
-    public GadgetNoMemory(String name, int code) {
-        super(name, code);
-        this.code = code;
-    }
-}
