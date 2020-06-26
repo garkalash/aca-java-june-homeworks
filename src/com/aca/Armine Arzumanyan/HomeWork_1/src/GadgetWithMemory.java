1b621a834bd8bc8cb11a26954829a639e2956545
Armine Arzumanyan <armine.arzumanyan@gmail.com>
2020-06-26 19:31:16 +0400

    Delete GadgetWithMemory.java

--- src/com/aca/Armine Arzumanyan/HomeWork_1/src/GadgetWithMemory.java
+++
@@ -1,9 +0,0 @@
-public class GadgetWithMemory extends Gadget {
-   int rum;
-
-    public GadgetWithMemory(String name, int code) {
-        super(name, code);
-        this.code = code;
-    }
-
-}
