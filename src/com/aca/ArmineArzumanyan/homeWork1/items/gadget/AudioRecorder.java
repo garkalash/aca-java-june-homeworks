c8cb0bb1d1507a200a95f9d5a0498e9d7080d7f5
ArzArm <armine.arzumanyan@gmail.com>
2020-07-02 12:52:26 +0400

    HomeWork2

--- src/com/aca/ArmineArzumanyan/homeWork1/items/gadget/AudioRecorder.java
+++ src/com/aca/ArmineArzumanyan/homeWork1/items/gadget/AudioRecorder.java
@@ -6,4 +6,5 @@ public interface AudioRecorder {
         MP3, WAV;
     }
 
+
 }
--- src/com/aca/ArmineArzumanyan/homeWork1/items/gadget/PenAudioRecorder.java
+++ src/com/aca/ArmineArzumanyan/homeWork1/items/gadget/PenAudioRecorder.java
@@ -1,6 +1,6 @@
 package com.aca.arminearzumanyan.homework1.items.gadget;
 
-public class PenAudioRecorder extends MemoryGadget implements AudioRecorder {
+public class PenAudioRecorder extends MemoryGadget implements AudioRecorder, PenRecorder {
 
     public PenAudioRecorder(String name, int count, int price, int recDuration, int memoryInGB) {
         super(name, count, price, recDuration, memoryInGB);
@@ -9,5 +9,10 @@ public class PenAudioRecorder extends MemoryGadget implements AudioRecorder {
     @Override
     public AudioType getAudioType() {
         return AudioType.MP3;
+    }
+
+    @Override
+    public WritingColor getWritingColor() {
+        return WritingColor.RAD_BLUE;
     }
 }
