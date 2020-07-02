6f86ac7e8f42c617799d17f7df55fbb97d3a9f53
HamoGhazoyan1 <hm.ghazoyan@gmail.com>
2020-07-02 16:02:39 +0400

    Homework 2 (except filereading)

--- src/com/aca/Client.java
+++ src/com/aca/Client.java
@@ -5,15 +5,16 @@ import com.aca.types.AudioRecorder;
 import com.aca.types.Gadget;
 import com.aca.types.VideoRecorder;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         List<Gadget> gadgets = new ArrayList<>();
 
-        PenVideoRecorder penVideoRecorder = new PenVideoRecorder(2,6500,10);
-        gadgets.add(penVideoRecorder);
+        RedPenAudioRecorder redPenAudioRecorderPenVideoRecorder = new RedPenAudioRecorder(2,6500,10,10);
+        gadgets.add(redPenAudioRecorderPenVideoRecorder);
         PenAudioRecorder penAudioRecorder = new PenAudioRecorder(9,14000,10,8);
         gadgets.add(penAudioRecorder);
         USBAudio usbAudio = new USBAudio(9,11000,10,8);
@@ -33,12 +34,16 @@ public class Client {
 //        manager.changePrice(watch,40000);
 //        System.out.println(watch.getPrice());
 //        System.out.println(Gadget.allItemsCount);
-        for (VideoRecorder allVideoRecorder : manager.getAllVideoRecorders()) {
-            System.out.println(allVideoRecorder.getClass());
-        }
-        for (AudioRecorder allAudioRecorder : manager.getAllAudioRecorders()) {
-            System.out.println(allAudioRecorder.getClass());
-        }
+//        for (VideoRecorder allVideoRecorder : manager.getAllVideoRecorders()) {
+//            System.out.println(allVideoRecorder.getClass());
+//        }
+//        for (AudioRecorder allAudioRecorder : manager.getAllAudioRecorders()) {
+//            System.out.println(allAudioRecorder.getClass());
+//        }
+        manager.testGadgets(gadgets);
+        System.out.println(manager.getWorkingGadgets());
+        System.out.println(manager.getNotWorkingGadgets());
+
     }
 
 }
