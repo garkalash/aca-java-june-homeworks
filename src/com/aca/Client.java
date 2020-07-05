01a986df1525bb3741a3ecf3046b86ccd63eed35
HamoGhazoyan1 <hm.ghazoyan@gmail.com>
2020-07-05 06:02:43 +0400

    Homework 2

--- src/com/aca/Client.java
+++ src/com/aca/Client.java
@@ -1,49 +1,83 @@
 package com.aca;
 
+import com.aca.exceptions.WrongProductData;
 import com.aca.items.*;
-import com.aca.types.AudioRecorder;
 import com.aca.types.Gadget;
-import com.aca.types.VideoRecorder;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Client {
-    public static void main(String[] args) throws FileNotFoundException {
-        List<Gadget> gadgets = new ArrayList<>();
 
-        RedPenAudioRecorder redPenAudioRecorderPenVideoRecorder = new RedPenAudioRecorder(2,6500,10,10);
-        gadgets.add(redPenAudioRecorderPenVideoRecorder);
-        PenAudioRecorder penAudioRecorder = new PenAudioRecorder(9,14000,10,8);
-        gadgets.add(penAudioRecorder);
-        USBAudio usbAudio = new USBAudio(9,11000,10,8);
-        gadgets.add(usbAudio);
-        USBVideo usbVideo  = new USBVideo(2,5000,10);
-        gadgets.add(usbVideo);
-        Watch watch  = new Watch(5,13000,10,8);
-        gadgets.add(watch);
-
+    public static void main(String[] args) throws IOException {
+        List<Gadget> gadgets = getProductList("aca-java-june-homeworks/src/com/aca/productList/product.csv");
         Manager manager = new Manager(gadgets);
-//        manager.passList(gadgets);
-//        manager.sellItem(usbAudio.getClass());
-//        manager.sellItem(usbVideo.getClass());
-//        manager.getListOfItems(gadgets);
-//        System.out.println(manager.getSoldAmount());
-//        System.out.println(watch.getPrice());
-//        manager.changePrice(watch,40000);
-//        System.out.println(watch.getPrice());
-//        System.out.println(Gadget.allItemsCount);
-//        for (VideoRecorder allVideoRecorder : manager.getAllVideoRecorders()) {
-//            System.out.println(allVideoRecorder.getClass());
-//        }
-//        for (AudioRecorder allAudioRecorder : manager.getAllAudioRecorders()) {
-//            System.out.println(allAudioRecorder.getClass());
-//        }
-        manager.testGadgets(gadgets);
-        System.out.println(manager.getWorkingGadgets());
-        System.out.println(manager.getNotWorkingGadgets());
 
+       manager.testGadgets(gadgets);
+        System.out.println(manager.getWorkingGadgets());
+       System.out.println(manager.getNotWorkingGadgets());
+        for (Gadget gadget : gadgets) {
+            System.out.println(gadget.getName());
+        }
+        manager.sellItem("Watch");
+
+    }
+    private static List<Gadget> getProductList(String listPath){
+        Path path = Paths.get(listPath);
+        Logger logger =Logger.getGlobal();
+        List<Gadget> gadgets = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(path.toFile()))){
+            String row;
+            while ((row = br.readLine()) != null){
+                String[] productData = row.split(";");
+                if(productData.length == 5){
+                    Integer duration = Integer.valueOf(productData[1].trim());
+                    Integer price = Integer.valueOf(productData[2].trim());
+                    Integer count = Integer.valueOf(productData[3].trim());
+                    Integer memory = Integer.valueOf(productData[4].trim());
+                    switch (productData[0]){
+                        case "PenVideoRecorder" :
+                            PenVideoRecorder penVideoRecorder = new PenVideoRecorder(duration,price,count,"PenVideoRecorder");
+                            gadgets.add(penVideoRecorder);
+                            break;
+                        case "BluePenAudioRecorder" :
+                            BluePenAudioRecorder bluePenAudioRecorder = new BluePenAudioRecorder(duration,price,count,memory,"BluePenAudioRecorder");
+                            gadgets.add(bluePenAudioRecorder);
+                            break;
+                        case "USBVideo" :
+                            USBVideo usbVideo = new USBVideo(duration,price,count,"USBVideo");
+                            gadgets.add(usbVideo);
+                            break;
+                        case "USBAudio" :
+                            USBAudio usbAudio = new USBAudio(duration,price,count,memory,"USBAudio");
+                            gadgets.add(usbAudio);
+                            break;
+                        case "RedPenAudioRecorder" :
+                            RedPenAudioRecorder redPenAudioRecorder = new RedPenAudioRecorder(duration,price,count,memory,"RedPenAudioRecorder");
+                            gadgets.add(redPenAudioRecorder);
+                            break;
+                        case "Watch" :
+                            Watch watch = new Watch(duration,price,count,memory,"Watch");
+                            gadgets.add(watch);
+                            break;
+                    }
+                }else {
+                    logger.warning("Wrong product data");
+                    throw new WrongProductData();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return gadgets;
     }
 
 }
