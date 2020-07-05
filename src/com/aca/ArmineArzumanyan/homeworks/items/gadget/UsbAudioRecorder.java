073126b2327520d2d9f43556381459617a50f50f
ArzArm <armine.arzumanyan@gmail.com>
2020-07-06 00:38:01 +0400

    HomeWork2 with using Generic method

--- src/com/aca/ArmineArzumanyan/homeworks/items/gadget/UsbAudioRecorder.java
+++ src/com/aca/ArmineArzumanyan/homeworks/items/gadget/UsbAudioRecorder.java
@@ -1,7 +1,5 @@
 package com.aca.arminearzumanyan.homeworks.items.gadget;
 
-import java.util.logging.Logger;
-
 public class UsbAudioRecorder extends MemoryGadget implements AudioRecorder {
 
     public UsbAudioRecorder(String name, int price, int recDuration, int memoryInGB) {
--- src/com/aca/ArmineArzumanyan/homeworks/manager/GadgetStoreManager.java
+++ src/com/aca/ArmineArzumanyan/homeworks/manager/GadgetStoreManager.java
@@ -5,6 +5,7 @@ import com.aca.arminearzumanyan.homeworks.exceptions.FileFormatException;
 import com.aca.arminearzumanyan.homeworks.items.gadget.*;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.logging.Logger;
@@ -21,7 +22,7 @@ public class GadgetStoreManager {
     private void writeInFile(Gadget gadget, File file) {
         try (FileWriter fw = new FileWriter(file, true)) {
             logger.info("Enter into try");
-            fw.write(gadget.getName() + ",  " +  "price: " + gadget.getPrice() + "\n");
+            fw.write(gadget.getName() + ",  " + "price: " + gadget.getPrice() + "\n");
         } catch (IOException ex) {
             logger.severe("An error occurred." + ex.getMessage());
             ex.printStackTrace();
@@ -46,10 +47,65 @@ public class GadgetStoreManager {
     }
 
 
+    public void createGadget1()  {
+        ArrayList<String> productsProperty = takeProductFromFile();
+
+        List<Class<? extends Gadget>> classList = new ArrayList<>();
+        classList.add(PenAudioRecorder.class);
+        classList.add(PenVideoRecorder.class);
+        classList.add(UsbAudioRecorder.class);
+        classList.add(UsbVideoRecorder.class);
+        classList.add(Watch.class);
+
+        for (int i = 0; i < productsProperty.size(); i++) {
+            String[] gadgetProperty = productsProperty.get(i).split(",");
+            if (gadgetProperty.length >= 5) {
+                String name = gadgetProperty[1];
+                int count = Integer.parseInt(gadgetProperty[2]);
+                int price = Integer.parseInt(gadgetProperty[3]);
+                int recDuration = Integer.parseInt(gadgetProperty[4]);
+                Integer code = Integer.parseInt(gadgetProperty[0]);
+                int memoryInGb = 0;
+                if (gadgetProperty.length == 6) {
+                    memoryInGb = Integer.parseInt(gadgetProperty[5]);
+                }
+                for (Class<? extends Gadget> aClass : classList) {
+                    if (name.equals(aClass.getSimpleName())) {
+                        Gadget gadget = null;
+                        if (memoryInGb == 0) {
+                            Class[] constructorParameters = new Class[]{String.class, int.class, int.class};
+                            try {
+                                gadget = aClass.getConstructor(constructorParameters).newInstance(name, price, recDuration);
+                            } catch (ReflectiveOperationException ex) {
+                                logger.severe("An error occurred." + ex.getMessage());
+                                ex.printStackTrace();
+                            }
+                            gadget.setCount(count);
+                            allItems.put(code, gadget);
+                            break;
+                        } else {
+                            Class[] parameters = new Class[]{String.class, int.class, int.class, int.class};
+                            try {
+                                gadget = aClass.getConstructor(parameters).newInstance(name, price, recDuration, memoryInGb);
+                            } catch (ReflectiveOperationException ex) {
+                                logger.severe("An error occurred." + ex.getMessage());
+                                ex.printStackTrace();
+                            }
+                            gadget.setCount(count);
+                            allItems.put(code, gadget);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
     public void createGadget() {
-        ArrayList<String> productProperties = takeProductFromFile();
-        for (int i = 0; i < 5; i++) {
-            String[] gadgetProperty = productProperties.get(i).split(",");
+        ArrayList<String> productsProperty = takeProductFromFile();
+        for (int i = 0; i < productsProperty.size(); i++) {
+            String[] gadgetProperty = productsProperty.get(i).split(",");
             if (gadgetProperty.length >= 5) {
                 String name = gadgetProperty[1];
                 int count = Integer.parseInt(gadgetProperty[2]);
@@ -110,7 +166,6 @@ public class GadgetStoreManager {
         for (Integer i : allItems.keySet()) {
             Gadget gadget = allItems.get(i);
             int l = gadget.getCount();
-            int notWorkingGadgetsCount = 0;
             for (int j = 1; j < l; j++) {
                 gadget.turnOnGadget();
                 if (isGadgetWorking()) {
@@ -119,7 +174,7 @@ public class GadgetStoreManager {
                 } else {
                     logger.info("Gadget is not working");
                     writeInFile(gadget, file);
-                    gadget.setCount(gadget.getCount()-1);
+                    gadget.setCount(gadget.getCount() - 1);
                 }
             }
 
