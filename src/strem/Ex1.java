ab081b54be777f7913a304d4630dd92772b13810
ArzArm <armine.arzumanyan@gmail.com>
2020-07-28 11:35:05 +0400

    HomeWork5 <Stream>

---
+++ src/strem/Ex1.java
@@ -0,0 +1,21 @@
+package strem;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+
+        List<Integer> list = Arrays.asList(33, 56, 4, 78);
+        List<String> stream = list.stream()
+                .map(s -> s % 2 == 0 ? "e" + s : "o" + s)
+                .collect(Collectors.toList());
+        System.out.println(stream);
+    }
+
+
+}
---
+++ src/strem/worldcountry/Main.java
@@ -0,0 +1,35 @@
+package strem.worldcountry;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Country armenia = new Country("ARM", "Armenia", 3000000, 101);
+        City yerevan = new City(101, "Yerevan", "ARM", 1060138);
+        City gyumri = new City(102, "Gyumri", "ARM", 118600);
+        City vanadzor = new City(103, "Vanadzor", "ARM", 78000);
+        armenia.getCities().addAll(Arrays.asList(yerevan,gyumri,vanadzor));
+
+        Country usa = new Country("USA", "United States of America", 331139904, 201);
+        City washington = new City(201, "Washington", "USA", 705750);
+        City newYork = new City(202, "New York", "USA", 8175133);
+        City atlanticCity = new City(203, "Atlantic City", "USA", 39558);
+        usa.getCities().addAll(Arrays.asList(washington,newYork,atlanticCity));
+
+        Country france = new Country("FR", "France", 65283584, 301);
+        City paris = new City(301, "Paris", "FR", 2150000);
+        City lyon = new City(302, "Lyon", "FR", 516092);
+        City nantes = new City(303, "Nantes", "FR", 309345);
+        france.getCities().addAll(Arrays.asList(paris,lyon,nantes));
+
+        System.out.println(armenia.getHighestPopulatedCity().toString());
+        System.out.println(france.getHighestPopulatedCity().toString());
+        System.out.println(usa.getHighestPopulatedCity().toString());
+
+
+    }
+
+
+}
