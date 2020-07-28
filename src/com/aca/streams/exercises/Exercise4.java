53e50e18b79c741560a436df50d4630b58a36652
garik <garik@jazva.com>
2020-07-28 18:31:28 +0400

    exercises for streams

---
+++ src/com/aca/streams/exercises/Exercise4.java
@@ -0,0 +1,13 @@
+package com.aca.streams.exercises;
+
+/**
+ * Sort the countries by their population densities in descending order ignoring zero population countries
+ *
+ * @author: garik
+ * @created: 7/28/2020, 6:22 PM
+ */
+public class Exercise4 {
+    public static void main(String[] args) {
+        //implementation here
+    }
+}
---
+++ src/com/aca/streams/persistance/WorldDao.java
@@ -0,0 +1,9 @@
+package com.aca.streams.persistance;
+
+/**
+ *
+ * @author garik
+ */
+public interface WorldDao extends CountryDao,CityDao {
+
+}
