0386d92059ea10630c2e812fac13fde4be8ee3e8
garik <garik@jazva.com>
2020-07-29 21:44:15 +0400

    model to tune your code

--- src/com/aca/streams/exercises/ExerciseOfDev.java
+++ src/com/aca/streams/exercises/ExerciseOfDev.java
@@ -37,5 +37,31 @@ public class ExerciseOfDev {
         return continentsMap;
     }
 
+    class ContinentPopulatedCity implements Map.Entry<String, City> {
+        private String continent;
+        private City city;
+
+        public ContinentPopulatedCity(String continent, City city) {
+            this.continent = continent;
+            this.city = city;
+        }
+
+        @Override    public String getKey() {
+            return continent;
+        }
+
+        @Override
+        public City getValue() {
+            return city;
+        }
+
+        @Override
+        public City setValue(City city) {
+            this.city = city;
+            return city;
+        }
+
+    }
+
 
 }
