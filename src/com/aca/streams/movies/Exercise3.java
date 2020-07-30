4336276ba838cf554d1e80db6792563d64595036
garik <garik@jazva.com>
2020-07-30 15:53:44 +0400

    new staff

---
+++ src/com/aca/streams/movies/Exercise3.java
@@ -0,0 +1,13 @@
+package com.aca.streams.movies;
+
+/**
+ * Group the movies by the year and list them
+ *
+ * @author: garik
+ * @created: 7/30/2020, 3:06 PM
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/aca/streams/service/MovieService.java
@@ -0,0 +1,32 @@
+package com.aca.streams.service;
+
+import com.aca.streams.models.Director;
+import com.aca.streams.models.Genre;
+import com.aca.streams.models.Movie;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface MovieService {
+    Movie findMovieById(int id);
+
+    Collection<Movie> findAllMovies();
+
+    Collection<Movie> findAllMoviesByYearRange(int fromYear, int toYear);
+
+    Collection<Movie> findAllMoviesByDirectorId(int directorId);
+
+    Collection<Movie> findAllMoviesByYearRangeAndGenre(String genre,
+                                                       int fromYear, int toYear);
+
+    Collection<Movie> findAllMoviesByGenre(String genre);
+
+    Movie addMovie(int id, String title, int year, String imdb,
+                   List<Genre> genres, List<Director> directors);
+
+    Genre findGenreByName(String genre);
+
+    Collection<Genre> findAllGenres();
+
+    Collection<Director> findAllDirectors();
+}
