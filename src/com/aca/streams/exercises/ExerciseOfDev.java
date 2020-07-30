196f44bf452631727cfb2ccb4cf1cf5bba7729e2
Narek Hakobyan <hakobyan94.nh@gmail.com>
2020-07-30 15:52:41 +0400

    StreamHomework
    Signed-off-by: Narek Hakobyan <hakobyan94.nh@gmail.com>

--- src/com/aca/streams/exercises/ExerciseOfDev.java
+++ src/com/aca/streams/exercises/ExerciseOfDev.java
@@ -1,5 +1,14 @@
 package com.aca.streams.exercises;
 
+import com.aca.streams.models.City;
+import com.aca.streams.models.Country;
+import com.aca.streams.persistance.CityDao;
+import com.aca.streams.persistance.CountryDao;
+import com.aca.streams.persistance.InMemoryWorldDao;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Find the highest populated capital city of each continent
  * you should collect <Continent, City> map
@@ -9,6 +18,28 @@ package com.aca.streams.exercises;
  */
 public class ExerciseOfDev {
     public static void main(String[] args) {
+        System.out.println(sortCountriesByCapitalsPopulation_Narek());
+    }
 
+    public static Map<String, City> sortCountriesByCapitalsPopulation_Narek() {
+        CountryDao countryDao = InMemoryWorldDao.getInstance();
+        Map<String, City> continentsHighestCapitals = new HashMap<>();
+
+        CityDao cityDao = InMemoryWorldDao.getInstance();
+
+        countryDao.getAllContinents().forEach(continent -> {
+                    Optional<City> optionalCity = countryDao.findAllCountries().parallelStream()
+                            .filter(Objects::nonNull)
+                            .filter(country -> country.getContinent().equals(continent))
+                            .map(country -> cityDao.findCityById(country.getCapital()))
+                            .filter(Objects::nonNull)
+                            .max(Comparator.comparingInt(city -> city.getPopulation()));
+                    if (optionalCity.isPresent()) {
+                        continentsHighestCapitals.put(continent, optionalCity.get());
+                    }
+                }
+        );
+
+        return continentsHighestCapitals;
     }
 }
