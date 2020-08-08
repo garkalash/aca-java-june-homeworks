7bbf2848b323a703596bc3dca08efe6009d475a6
ArzArm <armine.arzumanyan@gmail.com>
2020-08-08 11:01:23 +0400

    SalesService Task1 by Armine
    breaking fixes
    
    is added deserializer
    
    SalesService Task1 by Armine
    
    SalesService Tasks 1,6,11 by Armine
    
    SalesService Tasks 1,6,11,16 by Armine

--- src/main/java/com/aca/files/utility/JsonBuilder.java
+++ src/main/java/com/aca/files/utility/JsonBuilder.java
@@ -5,36 +5,60 @@ import com.aca.files.model.SoldItem;
 import com.aca.files.model.User;
 import com.google.gson.*;
 
-import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author: garik
  * @created: 8/8/2020, 10:44 AM
  */
 public final class JsonBuilder {
-    public static Gson GSON_INSTANCE(){
+
+    public static Gson GSON_INSTANCE() {
         GsonBuilder gsonBuilder = new GsonBuilder();
         JsonDeserializer<SoldItem> deserializer = (json, typeOfT, context) -> {
             JsonObject jsonObject = json.getAsJsonObject();
-
             User seller = new Gson().fromJson(jsonObject.get("seller").toString(), User.class);
             User buyer = new Gson().fromJson(jsonObject.get("buyer").toString(), User.class);
             String id = jsonObject.get("_id").getAsString();
-            BigDecimal price = new BigDecimal(jsonObject.get("price").getAsString().replace("$", "").replace(",", ""));
+            BigDecimal price = new BigDecimal(jsonObject.get("price").getAsString().replaceAll("[^0-9.]", ""));
+            LocalDateTime dateTime = LocalDateTime.parse(jsonObject.get("soldDate").getAsString().replace("Z", ""));
             Car car = new Gson().fromJson(jsonObject.get("car"), Car.class);
-
             SoldItem soldItem = new SoldItem();
             soldItem.setBuyer(buyer);
             soldItem.setCar(car);
             soldItem.setPrice(price);
             soldItem.setId(id);
             soldItem.setSeller(seller);
-
+            soldItem.setSoldDate(dateTime);
             return soldItem;
         };
         gsonBuilder.registerTypeAdapter(SoldItem.class, deserializer);
 
         return gsonBuilder.create();
     }
+
+    public static Gson GSON_INSTANCE2() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        JsonSerializer<SoldItem> serializer = (soldItem, type, jsonSerializationContext) -> {
+
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("id", soldItem.getId());
+            jsonObject.addProperty("price", soldItem.getPrice().toString().concat(" USD"));
+            jsonObject.addProperty("soldDate", soldItem.getSoldDate().format(DateTimeFormatter.ISO_DATE_TIME));
+            jsonObject.add("buyer", new Gson().toJsonTree(soldItem.getBuyer()));
+            jsonObject.add("seller", new Gson().toJsonTree(soldItem.getSeller()));
+            jsonObject.add("car", new Gson().toJsonTree(soldItem.getCar()));
+            return jsonObject;
+
+        };
+        gsonBuilder.registerTypeAdapter(SoldItem.class, serializer).create();
+
+        return gsonBuilder.create();
+    }
+
+
 }
